6239e763c7af13a52f0a98ec506826a2bf3b4752
Adam Smith <adam.smith.home@ntlworld.com>
2024-04-24 11:40:58 +0100

    add @tensor to apply

--- src/MPS/apply.jl
+++ src/MPS/apply.jl
@@ -6,15 +6,14 @@ function apply_1site!(mps::MPS, O::Array{<:Number,2}, site::Int)
     end
     movecentre!(mps, site)
 
-    C = mps.tensors[site]  # vl, p, vr
-    C = contract(O, C, 2, 2)  # p, vl, vr
-    mps.tensors[site] = permutedims(C, (2,1,3))  # vl, p, vr
+    @tensor mps.tensors[site][vl, p, vr] = O[p, pc] * mps.tensors[site][vl, pc, vr]
 end
 
 
 export apply_2site!
 """
 Apply 2 site gate to mps. Normalised truncation by default!
+Automatically flips left-right to use lexigraphical ordering.
 """
 function apply_2site!(mps::MPS, O::Array{<:Number,2}, site::Int; normalised::Bool=true)
     if site < 1 || site > length(mps)-1
@@ -26,33 +25,18 @@ function apply_2site!(mps::MPS, O::Array{<:Number,2}, site::Int; normalised::Boo
         movecentre!(mps, site+1)
     end
 
-    """O_swapped = reshape(O, (2,2,2,2))
-    O_swapped = permutedims(O_swapped, (2,1,4,3))
-    O_swapped = reshape(O_swapped, (4,4))
+    O_copy = reshape(copy(O), (2,2,2,2))  # por, pol, pir, pio  (flipped!)
 
-    A = copy(mps.tensors[site])  # vl, p, vr
-    B = copy(mps.tensors[site+1])  # vl, p, vr
-    C = contract(A, B, 3, 1)  # vl, p, p, vr
-    C = reshape(C, (size(C,1), size(C,2)*size(C,3), size(C,4)))  # vl, p*p, vr
-    C = contract(O_swapped, C, 2, 2)  # p, vl, vr
-    C = permutedims(C, (2,1,3))
-    vl = size(C,1)
-    vr = size(C,3)
-    C = reshape(C, (size(C,1)*mps.d, mps.d*size(C,3)))"""
+    @tensor theta[vl, pl, pr, vr] := mps.tensors[site][vl, pl, c] * mps.tensors[site+1][c, pr, vr]
+    @tensor theta[vl, pl, pr, vr] = O_copy[pr, pl, cr, cl] * theta[vl, cl, cr, vr]  # flip left and right for contraction!
+    vl = size(theta,1)
+    vr = size(theta,4)
+    theta = reshape(theta, (vl*mps.d, mps.d*vr))
 
-    O_copy = reshape(copy(O), (2,2,2,2))
-    A = mps.tensors[site]  # vl, p, vr
-    B = mps.tensors[site+1]  # vl, p, vr
-    A = contract(A, B, 3, 1)  # vl, pl, pr, vr
-    A = contract(O_copy, A, [4,3], [2,3])  # pr, pl, vl, vr
-    A = permutedims(A, (3,2,1,4))  # vl, pl, pr, vr
-    vl = size(A,1)
-    vr = size(A,4)
-    A = reshape(A, (vl*mps.d, mps.d*vr))
-
-    A, S, B = svd_truncated(A, mps.chiMax, mps.threshold; normalised=normalised)
+    A, S, B = svd_truncated(theta, mps.chiMax, mps.threshold; normalised=normalised)
     A = A * diagm(S)
 
     mps.tensors[site] = reshape(A, (vl,mps.d,size(A,2)))
     mps.tensors[site+1] = reshape(B, (size(B,1),mps.d,vr))
+    
 end
